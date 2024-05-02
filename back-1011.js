07d488272943d28cebfff52799a43825bd5c8699
iksoon <jjsk109@gmail.com>
2024-05-02 23:42:47 +0900

    타임아웃 개선

--- back-1011.js
+++ back-1011.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 // let input = fs.readFileSync("/dev/stdin").toString().split("\n");
-const input = ["3","0 3","1 5","45 50"];
+const input = ["3","10 30","0 2147483648","45 50"];
 let inputArr = [];
 for (let i = 1; i < input.length; i++) {
     const spl = input[i].split(" ");
@@ -8,14 +8,15 @@ for (let i = 1; i < input.length; i++) {
     inputArr.push(leng);
 }
 
-const answer = inputArr.map((n)=>{
+
+for (let k = 0; k < inputArr.length; k++) {
+    const n = inputArr[k];
     let last = 1;
     let secondLast = 0;
     let x = 1;
     const max = 2 ** 31;
-    
     let anw = 0;
-
+    let isCheck = false;
     for (let i = 1; i <= max; i++) {
         for (let j = 0; j < 2; j++) {
             x = x+i;
@@ -23,13 +24,34 @@ const answer = inputArr.map((n)=>{
             last = x;
             anw++;
             if (secondLast <= n && last > n ) {
-                return anw;
+                console.log(anw);
+                isCheck = true;
+                break;
             }
         }
+        if(isCheck){
+            break;
+        }
     }
-    return anw;
-});
-
-
-console.log(answer.join("\n"));
+    
+  
+}
+console.log();
+for (let k = 0; k < inputArr.length; k++) { 
+    const length = inputArr[k]; 
+    let pos = 1; 
+    let turn = 1; 
+    let pos_pow = 0; 
+    while(pos < length) { 
+        pos_pow++; 
+        turn++; 
+        pos += pos_pow; 
+        if(pos >= length) break; 
+        turn++; 
+        pos += pos_pow; 
+        if(pos >= length) break; 
+    } 
+    if(pos > length) turn--; 
+    console.log(turn); 
+} 
 
